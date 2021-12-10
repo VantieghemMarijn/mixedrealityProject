641bddffc3ae179cedc9800fc6ab187a4da17048
WoutDemeyere <wout.demeyere@student.howest.be>
2021-12-10 14:59:22 +0100

    added scroll view sort of

---
+++ Dikke AR App - FOODIE/Assets/Scripts/FlexibleUIData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flexible UI Data")]
+public class FlexibleUIData : ScriptableObject
+{
+
+}
+
---
+++ Dikke AR App - FOODIE/Assets/Scripts/FlexibleUiMenuItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(Text))]
+
+
+public class FlexibleUiMenuItem : FlexibleUI
+{
+   // Image image;
+   // Button buttonView, buttonAdd;
+   // Text title, desc, price;
+   // Canvas background;
+
+    //protected override void OnSkinUI()
+   //{
+
+       // base.OnSkinUI();
+
+        //image = GetComponent<Image>();
+       // Button = GetComponent<Button>();
+
+
+
+    //}
+
+    
+}
